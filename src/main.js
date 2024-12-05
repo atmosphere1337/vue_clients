b72ae0748e756568f8548d48c2d51a365454a9d3
Igor Boldyrev <agbdlcider@gmail.com>
2024-12-05 19:24:29 +0700

    1.2 Add FontAwesome

--- src/main.js
+++ src/main.js
@@ -1,15 +1,22 @@
+import { createApp } from 'vue'
+import App from './App.vue'
 import './assets/main.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 
 const vuetify = createVuetify({
     components,
     directives,
   })
 
-import { createApp } from 'vue'
-import App from './App.vue'
+library.add(fas)
 
-createApp(App).use(vuetify).mount('#app')
+createApp(App)
+    .use(vuetify)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
