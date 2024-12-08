081a0d7569a3365fdded63265e8e540ee9b8e000
Igor Boldyrev <agbdlcider@gmail.com>
2024-12-08 22:29:27 +0700

    1.3 Remove Vuetify

--- src/main.js
+++ src/main.js
@@ -1,22 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/main.css'
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 
-const vuetify = createVuetify({
-    components,
-    directives,
-  })
-
 library.add(fas)
 
 createApp(App)
-    .use(vuetify)
     .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app')
