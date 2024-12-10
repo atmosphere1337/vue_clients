4d6fde4a35c033b200fa4d327e4235fea4d252f9
Igor Boldyrev <agbdlcider@gmail.com>
2024-12-10 22:43:46 +0700

    3.2 create userStore

---
+++ src/stores/users.js
@@ -0,0 +1,64 @@
+import { defineStore } from 'pinia';
+import { saveLocalDataToBrowser, getLocalDataFromBrowser } from '@/utils/localStorageService';
+import { sortUserByRating, sortUsersBySecondName } from './utils/userSort';
+
+export const useUserStore = defineStore('users', {
+  state: () => {
+    return  {
+      apiUsers: [],
+      localUsers: null,
+      targetUser: null,
+      targetUserId: null,
+    }
+  },
+  getters: {
+    getUserIds: (state) => state.apiUsers.map(user => user.id),
+    getFullUsers: (state) => state.apiUsers.map(
+      user => ({...user, ...this.localUsers.find((local) => local.id === user.id)})
+    ),
+  },
+  actions: {
+    syncTargetUserWith(listMode) {
+      const {id, rating, comment} = this.targetUser;
+      this.localUsers .forEach((localUser, index) => {
+        if (localUser.id === id) {
+          this.localUsers[index] = {id, rating, comment} ;
+          if (listMode === 'rating') {
+            this.apiUsers = sortUserByRating(this.getFullUsers);
+          }
+          saveLocalDataToBrowser(this.localUsers);
+        }
+      })
+    },
+    syncBrowserDataAndState() {
+      this.localUsers = getLocalDataFromBrowser();
+
+      const browserData = getLocalDataFromBrowser()
+      if (browserData === undefined || browserData === null) {
+        saveLocalDataToBrowser(
+          this.apiUsers.map(user => ({id: user.id, rating: 0, comment: ''}))
+        );
+      }
+      if (this.localUsers === null) {
+        this.localUsers = getLocalDataFromBrowser();
+        return;
+      } 
+      else {
+        for (const item of this.apiUsers) {
+          if (!this.getUserIds.find((id) => id === item.id)) {
+            this.localUsers.push(
+              {id: item.id, rating: 0, comment: ''}
+            );
+          }
+        }
+        saveLocalDataToBrowser(this.localUsers);
+      }
+    },
+    sortUsers(listMode) {
+      this.apiUsers = (listMode === 'clients')
+        ? sortUsersBySecondName(this.apiUsers)
+        : sortUserByRating(this.getFullUsers);
+    }
+
+  }
+});
